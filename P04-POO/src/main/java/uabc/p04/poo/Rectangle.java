9e100b0e15aa3059b93dd06bd73957465874b325
Leonel Araujo <oscar.araujo@uabc.edu.mx>
2023-10-10 23:21:48 -0700

    Commit con las clases necesarias para la practica

---
+++ P04-POO/src/main/java/uabc/p04/poo/Rectangle.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uabc.p04.poo;
+
+/**
+ *
+ * @author Dell
+ */
+public class Rectangle {
+    private int xPos;
+    private int yPos;
+    private int width;
+    private int height;
+
+    public Rectangle(int xPos, int yPos, int width, int height) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle() {
+    }
+    
+}
